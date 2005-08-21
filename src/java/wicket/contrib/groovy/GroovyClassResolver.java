c504e517ac18a581956b7f491b3e9e6c7e389ef1
joco01 <joco01@ef7698a4-5110-0410-9fc6-c7eb3693863f>
2005-08-21 15:25:34 +0000

    added classloader to locate
    git-svn-id: https://wicket-stuff.svn.sourceforge.net/svnroot/wicket-stuff/trunk/wicket-contrib-groovy@250 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- src/java/wicket/contrib/groovy/GroovyClassResolver.java
+++ src/java/wicket/contrib/groovy/GroovyClassResolver.java
@@ -97,7 +97,7 @@ public class GroovyClassResolver implements IClassResolver
 		}
 
 		// Else, try Groovy.
-		final IResourceStream resource = application.getResourceStreamLocator().locate(classname.replace('.', '/'),
+		final IResourceStream resource = application.getResourceStreamLocator().locate(getClass().getClassLoader(),classname.replace('.', '/'),
 				null, null, ".groovy");
 		if (resource != null)
 		{
